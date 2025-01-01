bf3614536d285e49c3cf652177a305ef229c9247
Paul Hovey <paul@paulhovey.org>
2025-01-01 16:45:39 -0500

    apparently default doesn't exist?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as FileExplorer } from "./FileExplorer.svelte";
-export { default as Tree } from "./Tree.svelte";
+export FileExplorer from "./FileExplorer.svelte";
+export Tree from "./Tree.svelte";
 export { DIRECTORY, FILE } from "./types";
